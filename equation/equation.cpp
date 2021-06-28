64732970d212a9105d128d31e4410fe98c71ad22
Sarmouts <saradis.m06@gmail.com>
2021-06-28 12:40:16 +0300

    b

--- equation/equation.cpp
+++ equation/equation.cpp
@@ -21,7 +21,7 @@ int main()
 		{
 			cout << "x is Infinity in R " << "\n";
 		}
-		else if (a == 0 && c != 0 )
+		else if (a == 0 && c != 0)
 		{
 			cout << "x is Infinity in R " << "\n";
 		}
@@ -51,9 +51,13 @@ int main()
 		cin >> a;
 		cout << "Enter b" << "\n";
 		cin >> b;
-		if (a == 0 && b == 0 || a != 0 && b == 0 || a == 0 && b != 0)
+		if (a == 0 && b == 0)
 		{
 			cout << "Tautothta(x could be any number in R)" << "\n";
+		}
+		else if (a != 0 && b == 0 || a == 0 && b != 0)
+		{
+			cout << "x is equal to 0 " << "\n";
 		}
 		else {
 			x1 = 0;
@@ -73,21 +77,53 @@ int main()
 		cout << "Enter c" << "\n";
 		cin >> c;
 		d = b * b - 4 * a * c;
-		if (d < 0)
+		if (a == 0 && b == 0 && c == 0)
 		{
-			cout << "x is Infinity in R " << "\n";
+
+			cout << "The equation is an identity: it is true for all values " << "\n";
+
+		}
+		else if (a == 0 && b == 0 && c != 0)
+		{
+			cout << "The equation is a contradiction in R: it has no solutions " << "\n";
+		}
+		else if (a == 0 && b != 0 && c == 0 || a == 0 && b != 0 && c != 0)
+		{
+			x = -c / b;
+			cout << "x is equal to " << x << "\n";
+		}
+		else if (a != 0 && b == 0)
+		{
+			x = sqrt(-c / a);
+			if (x == 0)
+			{
+				x = 0;//Μπαινει γιατι εκτυπωνε -0 λογω του -c στη γραμμη 97  
+				cout << "x is equal to " << x << "\n";
+			}
+			else {
+				cout << "x is equal to +- " << x << "\n";
+			}
+
+		}
+		else if (d < 0)
+		{
+			cout << "D=" << d << "<0 " << "x is Infinity in R " << "\n" << endl;
+			x1 = (-b - sqrt(-d)) / (2 * a);
+			x2 = (-b + sqrt(-d)) / (2 * a);
+			cout << "x is equal to " << x1 << " or " << x2 << " in C " << "\n";
+
 		}
 		else if (d == 0)
 		{
 			x1 = (-b - sqrt(d)) / (2 * a);
 			x2 = (-b + sqrt(d)) / (2 * a);
-			cout << "x is equal to " << x1 << " (double solution) " << "\n";
+			cout << "D=" << d << "x is equal to " << x1 << " (double solution) " << "\n";
 		}
 		else
 		{
 			x1 = (-b - sqrt(d)) / (2 * a);
 			x2 = (-b + sqrt(d)) / (2 * a);
-			cout << "x is equal to " << x1 << " or " << x2 << "\n";
+			cout << "D=" << d << ">0 " << "x is equal to " << x1 << " or " << x2 << "\n";
 
 		}
 
